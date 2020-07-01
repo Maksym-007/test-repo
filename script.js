58aa12b2482eb12527ede2b1010e3446f63ae1f3
Maksym Kasilov <kasilmaxim@gmail.com>
2020-07-01 10:52:34 +0300

    classTask#3

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.group("Hola!");
-console.log("Hello world!");
+let age = +prompt('Сколько вам лет?');
+if (age >= 18) {
+    alert('Доступ к материалам открыт!');
+} else if (age <= 12) {
+    alert('В доступе отказано!');
+} else if (age > 12 || age < 18) {
+    alert('Вам доступна ознакомительнав версия!');
+}
+else {
+    alert('чтото пошло не так! Попробуйте снова!');
+}
+
